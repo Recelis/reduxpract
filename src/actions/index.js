452df32dc95ca66af89b657f27b210e4ac798a21
Jacky Lui <jackymlui@live.com>
2018-08-16 22:56:37 +1000

    converting to todoApp basic, completed to reducer composition section

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,35 @@
-export const CHANGE_PAGE_STATUS = "CHAGE_PAGE_STATUS";
+// actions types
+export const ADD_TODO = "ADD_TODO"; // action is explicitly declared using string literal constants, not necessary but very helpful to do this
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
 
-export function changePageStatus(state){
+/* some other constants */
+export const VisibilityFilters = {
+    SHOW_ALL:"SHOW_ALL",
+    SHOW_COMPLETED:"SHOW_COMPLETED",
+    SHOW_ACTIVE:"SHOW_ACTIVE"
+}
+
+// action creators
+// they simply are functions that return an action
+export function addTodo(text){
     return{
-        type: CHANGE_PAGE_STATUS,
-        pageStatus:state
+        type: ADD_TODO,
+        text
+    } // what does text do when it is like this? New ES6 syntax?
+}
+
+export function toggleTodo(index){
+    return {
+        type:TOGGLE_TODO,
+        index
+    }
+} 
+
+export function setVisibilityFilter(filter){
+    return{
+        type:SET_VISIBILITY_FILTER, filter
     }
 }
+
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,79 @@
-const CHANGE_PAGE_STATUS = "CHANGE_PAGE_STATUS";
+// reducers actually list out what happens to the state in store when the actions are called. 
+/*
+At this stage want to know what the shape of the state will be. Need to plan this out!
+{
+  visibilityFilter: 'SHOW_ALL',
+  todos: [
+    {
+      text: 'Consider using Redux',
+      completed: true
+    },
+    {
+      text: 'Keep all state in a single tree',
+      completed: false
+    }
+  ]
+}
+Want state to be as "normalized" as possible, which means as little nesting as possible!
+Would be better if each todo was identified with its own ID, but this example is simple. 
+*/
+
+// reducer is a pure function (which means that return value does not depend on local or external state and no side effect)  
+// it basically takes the previous state and an action and spits out the next state. 
+// never mutate reducer's arguments
+// perform side effects like API calls or routing transitions
+// call non-pure functins inside reducer.
+
+import { 
+    ADD_TODO, 
+    TOGGLE_TODO, 
+    VisibilityFilters, 
+    SET_VISIBILITY_FILTER 
+} from '../actions/index' // leave gap in between when importing so that it looks clear
 
 const initialState = {
-    pageStatus: 'loading',
+    visibilityFilter:VisibilityFilters.SHOW_ALL,
+    todos: []
 }
 
-export default function changePageStatus (state = initialState, action){
-    switch (action.type) {
-        case CHANGE_PAGE_STATUS:
-            return {
-                ...state, 
-                pageStatus:action.pageStatus
-            }
-        default: 
-        pageStatus: {state}
-    }
-}
+// without ES6 'default arguments syntax' https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Functions/default_parameters
+// function todoApp(state,action){
+//     if (typeof state === undefined){ // for the initial state
+//         return initialState
+//     }
+//     return state
+// }
+
+/* Before Reducer Composition (i.e. taking all unnecessary code in todosApp and putting it into one function to clean up)*/
+// function todoApp(state = initialState,action){ // ES6 voodoo magic that if state is undefined set to initial State
+//     switch(action.type){ // this is a property of action, which returns an object containing type, index, text and all of the properties you set.
+//         case SET_VISIBILITY_FILTER:
+//             return Object.assign({}, state,{
+//                 visibilityFilter:action.filter
+//             }) // this merges the empty object with state and the new action, which changes the visibilityFilter. The empty object first arg is the target 
+//         case ADD_TODO:
+//             return Object.assign({}, state, {
+//                 todos:[
+//                     ...state.todos, // spread operator as state.todos will come as its own array
+//                     {
+//                         text:action.text,
+//                         completed:false
+//                     } // the todo items is an object that contains two properties, text and completed. The Object.assign has merged the new item with the existing
+//                 ]
+//             })
+//         case TOGGLE_TODO:
+//             return Object.assign({}, state,{
+//                 todos:state.todos.map((todo,index)=>{ // map creates a new array and returns it, so there is no mutation
+//                     if (index === action.index){
+//                         return Object.assign({},todo,{ // this is the todo item object, not the list, which gets assigned into its own todo item 
+//                             completed:!todo.completed
+//                         })
+//                     }
+//                 })
+//             })
+//         default:
+//             return state // very important to return state if action.type is unknown
+//     }
+// }
+
 
